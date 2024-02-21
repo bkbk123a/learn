8386f7e5272db9237d7b70a5020dd967d69c223b
bkbk123a <qjarud32@naver.com>
2024-02-21 17:12:45 +0900

    AddPassesTasklet 분할
    passService 추가

---
+++ SpringBatch/src/main/java/com/example/batch/service/PassService.java
@@ -0,0 +1,79 @@
+package com.example.batch.service;
+
+import com.example.batch.entity.pass.Pass;
+import com.example.batch.entity.pass.UserGroup;
+import com.example.batch.entity.pass.UserPass;
+import com.example.batch.enumerator.PassStatus;
+import com.example.batch.repository.pass.PassRepository;
+import com.example.batch.repository.pass.UserGroupRepository;
+import com.example.batch.repository.pass.UserPassRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class PassService {
+  private final PassRepository passRepository;
+  private final UserPassRepository userPassRepository;
+  private final UserGroupRepository userGroupRepository;
+
+  /**
+   * 진행중인 이용권 조회
+   *
+   * @param status    이용권 상태
+   * @param startedAt 시작 시간
+   * @return          진행중인 이용권
+   */
+  public List<Pass> getNowPasses(PassStatus status, LocalDateTime startedAt) {
+    return passRepository
+        .findByPassStatusAndStartedAtLessThanAndExpiredAtGreaterThan(status, startedAt, startedAt);
+  }
+
+  public List<Integer> getUserGroupIdsFromPasses(List<Pass> passes) {
+    return passes.stream()
+        .map(Pass::getUserGroupId)
+        .toList();
+  }
+
+  /**
+   * 이용권 지급
+   *
+   * @param passes     진행중인 이용권
+   * @param userGroups 이용권 제공할 유저 그룹
+   * @return           유저에게 지급한 이용권 수
+   */
+  public long addPasses(List<Pass> passes, List<UserGroup> userGroups) {
+    List<UserPass> userPasses = new ArrayList<>();  // 유저에게 지급할 이용권
+    for (Pass pass : passes) {
+      List<Long> userIds = getUserIdsFromUserGroups(pass, userGroups);
+
+      for (Long userId : userIds) {
+        userPasses.add(UserPass.of(userId, pass));
+      }
+
+      pass.setPassStatus(PassStatus.COMPLETED);
+    }
+
+    passRepository.saveAll(passes);
+    return userPassRepository.saveAll(userPasses).size();
+  }
+
+  /**
+   * 유저 ID 획득
+   *
+   * @param pass 진행중인 이용권
+   * @param userGroups 유저 그룹
+   * @return 유저 그룹에서 진행중인 이용권에 해당하는 유저 ID
+   */
+  private List<Long> getUserIdsFromUserGroups(Pass pass, List<UserGroup> userGroups) {
+    return userGroups.stream()
+        .filter(u -> Objects.equals(u.getUserGroupId(), pass.getUserGroupId()))
+        .map(UserGroup::getUserId)
+        .toList();
+  }
+}
