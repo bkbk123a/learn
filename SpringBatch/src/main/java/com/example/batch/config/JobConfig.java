d81dcfcf9c633dc8454bb9899c808993071c9932
bkbk123a <qjarud32@naver.com>
2024-03-01 01:51:03 +0900

    JobConfig 주석 해제

--- SpringBatch/src/main/java/com/example/batch/config/JobConfig.java
+++ SpringBatch/src/main/java/com/example/batch/config/JobConfig.java
@@ -1,19 +1,19 @@
-//package com.example.batch.config;
-//
-//import org.springframework.batch.core.configuration.JobRegistry;
-//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-////@EnableBatchProcessing // Spring batch 실행 (spring 3.x.x 에사부터 사용 안함)
-//public class JobConfig {
-//
-//    @Bean
-//    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
-//        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
-//        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
-//        return jobRegistryBeanPostProcessor;
-//    }
-//}
+package com.example.batch.config;
+
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @EnableBatchProcessing // Spring batch 실행 (spring 3.x.x 에사부터 사용 안함)
+// controller 에서 jobRegistry.getJob()시 필요하다
+@Configuration
+public class JobConfig {
+    //
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
+    }
+}
